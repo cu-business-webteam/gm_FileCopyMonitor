d0281cbbc261b10be17c72edf447bd8ac00001f1
uniblab <tjb284@cornell.edu>
2018-01-24 09:24:33 -0500

    initial seed
    Signed-off-by: uniblab <tjb284@cornell.edu>

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Johnson.FileCopyMonitor {
+
+	public static class Program {
+
+		public static System.Int32 Main( System.String[] args ) {
+			throw new System.NotImplementedException();
+		}
+
+	}
+
+}
